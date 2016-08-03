88392c3b19e6549ac2beae961ef508648d527ac8
H3ADLESS <headless705@gmail.com>
2016-08-03 13:36:01 +0200

    FE-26, FE-27, FE-28, FE-31, FE-32: Initial commit of Sensor-Client App.
    Change-Id: Ibe7f68f7f18b5afe533d9098397ed7df0b4ab765

---
+++ app/src/main/java/de/h3adless/gpstracker/AppSettings.java
@@ -0,0 +1,16 @@
+package de.h3adless.gpstracker;
+
+/**
+ * Created by timo_ on 09.08.2016.
+ */
+public class AppSettings {
+
+    public static String SERVER_URL = null;
+    public static String SERVER_PORT = null;
+
+    public static long TRACKING_INTERVALL = 10000;
+    public static int SEND_EVERY_XTH_LOC = 1;
+
+    public static String RANDOM_DEVICE_UUID = null;
+
+}
