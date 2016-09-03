6617f5fc545b3f2a98f7ae658821b3da3ef036eb
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2016-09-03 17:29:33 +0200

    Experiment für orientation tracking
    Change-Id: Iab09043060d54540e4527299ff6e6117a07eafe8

---
+++ app/src/main/java/de/h3adless/gpstracker/activities/OrientationActivity.java
@@ -0,0 +1,60 @@
+package de.h3adless.gpstracker.activities;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+public class OrientationActivity extends Activity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    Sensor accelerometer;
+    Sensor magnetometer;
+    float bearing = 0.0f;
+    float pitch = 0.0f;
+    float roll = 0.0f;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(mCustomDrawableView);    // war mir nicht sicher wo das hin soll (am sinnvollsten wäre es das im main anzuzeigen)
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {  }
+
+    float[] mGravity;
+    float[] mGeomagnetic;
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+        if (mGravity != null && mGeomagnetic != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                bearing = orientation[0];
+                pitch = orientation[1];
+                roll = orientation[2];
+            }
+        }
+    }
+}
